40368f30a51d65345db8f295bf31dd293a0bf35e
Enrique Castellanos <enriquecastellanos2403@gmail.com>
2025-05-10 12:06:27 -0600

    modicacion server.js e index.html

--- server.js
+++ server.js
@@ -7,14 +7,19 @@ const PORT = 3000;
 
 // Middleware para parsear JSON
 app.use(express.json());
-app.use(express.static('public'));  // Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));  // Servir archivos estáticos desde la carpeta 'public'
+
+// Ruta para servir index.html en la raíz
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Ruta para recibir los datos de ubicación y guardarlos en un archivo JSON
 app.post('/save-location', (req, res) => {
     const locationData = req.body;
 
     // Ruta del archivo JSON donde se guardarán las ubicaciones
-    const filePath = path.join(__dirname, 'location-data.json');
+    const filePath = path.join(__dirname, 'public', 'locations.json');
 
     // Leer los datos previos (si existen) y agregar la nueva ubicación
     fs.readFile(filePath, 'utf8', (err, data) => {
